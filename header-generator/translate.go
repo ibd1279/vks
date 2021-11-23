91d9b9c8375b620c2715f5655dbd8f5775f49555
Jason Watson <jwatson@slashopt.net>
2021-11-23 08:15:25 +0100

    Moving code before adding config support.

--- header-generator/translate.go
+++ header-generator/translate.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
 	"strings"
+
+	strip "github.com/grokify/html-strip-tags-go"
 )
 
 type Translator interface {
@@ -331,4 +335,362 @@ type CommandData struct {
 type CommandParamData struct {
 	Name Translator // e.g. pName
 	Type Translator // e.g. string
+}
+
+func CommandToData(node *RegistryNode, command CommandElement) interface{} {
+	if len(command.Alias()) > 0 {
+		// TODO add support for Aliases later.
+		return nil
+	} else {
+		var returnTranslator Translator
+		if scalarXl8r := GetScalarTranslator(command.Proto.Type); scalarXl8r != nil {
+			returnTranslator = scalarXl8r
+		} else {
+			returnTranslator = helperMemberTypeTranslator(command.Proto.Type, "", command.Proto.Raw, "")
+		}
+		data := CommandData{
+			Name:   &ExportTranslator{&IdentifierTranslator{command.Name()}},
+			Return: returnTranslator,
+		}
+
+		for _, v := range command.Params {
+			data.Parameters = append(data.Parameters, CommandParamData{
+				Name: &NameTranslator{&LiteralTranslator{v.Name}},
+				Type: helperMemberTypeTranslator(v.Type, v.Length, v.Raw, ""),
+			})
+		}
+
+		return &struct {
+			Template string
+			Data     CommandData
+		}{"command", data}
+	}
+	return nil
+}
+
+func TypeToData(node *RegistryNode, tiepuh TypeElement) interface{} {
+	switch tiepuh.Category {
+	case TypeCategoryBasetype:
+		return baseTypeToData(node, tiepuh)
+	case TypeCategoryHandle:
+		return handleTypeToData(node, tiepuh)
+	case TypeCategoryEnum:
+		return enumTypeToData(node, tiepuh)
+	case TypeCategoryBitmask:
+		return enumTypeToData(node, tiepuh)
+	case TypeCategoryFuncpointer:
+		return funcTypeToData(node, tiepuh)
+	case TypeCategoryStruct:
+		return structTypeToData(node, tiepuh)
+	case TypeCategoryUnion:
+		return unionTypeToData(node, tiepuh)
+	}
+	return nil
+}
+
+func baseTypeToData(node *RegistryNode, tiepuh TypeElement) *struct {
+	Template string
+	Data     BaseData
+} {
+	if len(tiepuh.TypeTag) == 0 {
+		return nil
+	}
+	data := BaseData{
+		Type: GetScalarTranslator(tiepuh.TypeTag),
+		Name: &IdentifierTranslator{tiepuh.Name()},
+	}
+	return &struct {
+		Template string
+		Data     BaseData
+	}{"base", data}
+}
+
+func handleTypeToData(node *RegistryNode, tiepuh TypeElement) *struct {
+	Template string
+	Data     HandleData
+} {
+	var data HandleData
+	if t := GetHandleTranslator(tiepuh.Name()); t != nil {
+		data.Name = t
+	} else {
+		t = &HandleTranslator{tiepuh.Name()}
+		handleTranslatorList[tiepuh.Name()] = t
+		data.Name = t
+	}
+	return &struct {
+		Template string
+		Data     HandleData
+	}{"handle", data}
+}
+
+func enumTypeToData(node *RegistryNode, tiepuh TypeElement) *struct {
+	Template string
+	Data     EnumData
+} {
+	ed := EnumData{
+		Name: &IdentifierTranslator{tiepuh.Name()},
+	}
+
+	templateName := "enum"
+	if tiepuh.Category == TypeCategoryBitmask {
+		ed.Type = &IdentifierTranslator{tiepuh.TypeTag}
+		templateName = "bitmask"
+	}
+	for _, v := range node.EnumsParents() {
+		var bitmask bool
+		if v.E.Type == EnumsElementBitmask {
+			bitmask = true
+		}
+		var largestValue, smallestValue int64
+		for _, v := range v.N.EnumParents() {
+			evd := EnumValueData{
+				Type: ed.Name,
+				Name: &NameTranslator{&LiteralTranslator{v.E.Name}},
+			}
+			var valueTranslator Translator
+			if bitmask {
+				if len(v.E.Alias) > 0 {
+					valueTranslator = &LiteralTranslator{v.E.Alias}
+					evd.Alias = true
+				} else if v.E.BitPos == 0 && len(v.E.Value) > 0 {
+					valueTranslator = &ValueTranslator{v.E.Value}
+				} else {
+					valueTranslator = &BitValueTranslator{v.E.BitPos}
+				}
+			} else {
+				if val, _ := strconv.ParseInt(v.E.Value, 10, 64); val > largestValue || val < smallestValue {
+					if val > largestValue {
+						largestValue = val
+					} else if val < smallestValue {
+						smallestValue = val
+					}
+				}
+				if len(v.E.Alias) > 0 {
+					valueTranslator = &LiteralTranslator{v.E.Alias}
+					evd.Alias = true
+				} else {
+					valueTranslator = &ValueTranslator{v.E.Value}
+				}
+			}
+			evd.Value = valueTranslator
+			ed.Values = append(ed.Values, evd)
+		}
+		if smallestValue < 0 {
+			ed.Type = Int32Translator
+		}
+	}
+	if ed.Type == nil {
+		ed.Type = Uint32Translator
+	}
+
+	return &struct {
+		Template string
+		Data     EnumData
+	}{templateName, ed}
+}
+
+func funcTypeToData(node *RegistryNode, tiepuh TypeElement) *struct {
+	Template string
+	Data     FuncData
+} {
+	data := FuncData{
+		Type: GetScalarTranslator("void*"),
+		Name: &IdentifierTranslator{tiepuh.Name()},
+	}
+	return &struct {
+		Template string
+		Data     FuncData
+	}{"func", data}
+}
+
+func structTypeToData(node *RegistryNode, tiepuh TypeElement) *struct {
+	Template string
+	Data     StructData
+} {
+	data := StructData{
+		Name:     &StructTranslator{tiepuh.Name()},
+		ReadOnly: tiepuh.ReturnedOnly,
+	}
+	if len(tiepuh.Alias) > 0 {
+		data.Alias = &StructTranslator{tiepuh.Alias}
+		return &struct {
+			Template string
+			Data     StructData
+		}{"structalias", data}
+	}
+
+	for _, v := range tiepuh.StructMembers {
+		subData := StructMemberData{
+			Name: &NameTranslator{&ExportTranslator{&LiteralTranslator{v.Name}}},
+			Type: helperMemberTypeTranslator(v.Type, v.Length, v.Raw, v.Comment),
+		}
+		if len(v.Values) > 0 && len(strings.Split(v.Values, ",")) == 1 && v.Name == "sType" {
+			subData.Value = &LiteralTranslator{v.Values}
+		} else if strings.ContainsAny(subData.Type.CGo(), "[]") {
+			subData.Copy = true
+		}
+		data.Members = append(data.Members, subData)
+	}
+	return &struct {
+		Template string
+		Data     StructData
+	}{"struct", data}
+}
+
+func unionTypeToData(node *RegistryNode, tiepuh TypeElement) *struct {
+	Template string
+	Data     UnionData
+} {
+	data := UnionData{
+		Name: &UnionTranslator{tiepuh.Name()},
+	}
+	for _, v := range tiepuh.StructMembers {
+		data.Members = append(data.Members, UnionMemberData{
+			Name: &NameTranslator{&ExportTranslator{&LiteralTranslator{v.Name}}},
+			Type: helperMemberTypeTranslator(v.Type, v.Length, v.Raw, v.Comment),
+		})
+	}
+	return &struct {
+		Template string
+		Data     UnionData
+	}{"union", data}
+}
+
+var arrayNumericSizeRegexp *regexp.Regexp = regexp.MustCompile("\\[\\d+\\]")
+var arrayConstSizeRegexp *regexp.Regexp = regexp.MustCompile("\\[[^\\]]+\\]")
+
+func helperMemberTypeTranslator(specType, length, raw, comment string) Translator {
+	if len(specType) == 0 {
+		specType = "uint32_t"
+	}
+	if len(comment) > 0 {
+		raw = strings.ReplaceAll(raw, fmt.Sprintf("<comment>%s</comment>", comment), "")
+	}
+
+	pointerCount := len(strings.Split(raw, "*")) - 1
+	if pointerCount > 0 && specType == "void" {
+		specType = "void*"
+	}
+	sliceCount := 0
+	if len(length) > 0 {
+		lengthValues := strings.Split(length, ",")
+		sliceCount = len(lengthValues)
+		for _, v := range lengthValues {
+			v = strings.TrimSpace(v)
+			if v == "null-terminated" {
+				sliceCount = sliceCount - 1
+			} else if v == "1" {
+				sliceCount = sliceCount - 1
+			}
+		}
+	}
+	if len(strings.Split(raw, "[")) > 1 {
+		sliceCount = sliceCount + 1
+	}
+	pointerCount = pointerCount - sliceCount
+	if specType == "void*" {
+		if pointerCount > 0 {
+			pointerCount = pointerCount - 1
+		} else {
+			sliceCount = sliceCount - 1
+		}
+	}
+
+	arraySize := arrayNumericSizeRegexp.FindString(raw)
+	if sliceCount > 0 && len(arraySize) > 2 {
+		sliceCount = sliceCount - 1
+		arraySize = arraySize[1 : len(arraySize)-1]
+	} else if arraySize = arrayConstSizeRegexp.FindString(raw); sliceCount > 0 && len(arraySize) > 2 {
+		sliceCount = sliceCount - 1
+		arraySize = arraySize[1 : len(arraySize)-1]
+		arraySize = strip.StripTags(arraySize)
+	} else {
+		arraySize = ""
+	}
+
+	var typeTranslator Translator
+	var typeConverter Converter
+	if handleXl8r := GetHandleTranslator(specType); handleXl8r != nil {
+		typeTranslator = handleXl8r
+		typeConverter = handleXl8r
+	} else if scalarXl8r := GetScalarTranslator(specType); scalarXl8r != nil {
+		typeTranslator = scalarXl8r
+		typeConverter = scalarXl8r
+	} else {
+		typeTranslator = &IdentifierTranslator{specType}
+		typeConverter = &IdentifierTranslator{specType}
+	}
+	for h := 0; h < pointerCount; h++ {
+		typeTranslator = &PointerTranslator{typeTranslator, typeConverter}
+		typeConverter = &PointerTranslator{typeTranslator, typeConverter}
+	}
+	for h := 0; h < sliceCount; h++ {
+		typeTranslator = &SliceTranslator{typeTranslator, typeConverter}
+		typeConverter = &SliceTranslator{typeTranslator, typeConverter}
+	}
+	if len(arraySize) > 0 {
+		typeTranslator = &ArrayTranslator{
+			typeTranslator,
+			&ValueTranslator{arraySize},
+			typeConverter,
+		}
+	}
+
+	return typeTranslator
+}
+
+func ConstToData(node *RegistryNode) *struct {
+	Template string
+	Data     []ConstData
+} {
+	list := make([]ConstData, 0, len(node.Parents))
+	for _, v := range node.EnumsParents() {
+		for _, v := range v.N.EnumParents() {
+			var data *ConstData
+			if len(v.E.Name) > 0 && len(v.E.Value) > 0 {
+				var value = v.E.Value
+				if (strings.HasSuffix(value, "F") || strings.HasSuffix(value, "f")) && !(strings.HasPrefix(value, "0x") || strings.HasPrefix(value, "0X")) {
+					value = value[:len(value)-1]
+				}
+				if strings.HasPrefix(value, "(") && strings.HasSuffix(value, ")") {
+					value = value[1 : len(value)-1]
+				}
+				var ulonglong bool
+				if strings.HasSuffix(value, "ULL") || strings.HasSuffix(value, "ull") {
+					ulonglong = true
+					value = value[:len(value)-3]
+				}
+				var ulong bool
+				if strings.HasSuffix(value, "U") || strings.HasSuffix(value, "u") {
+					ulong = true
+					value = value[:len(value)-1]
+				}
+				if strings.HasPrefix(value, "~") {
+					if ulonglong {
+						value = fmt.Sprintf("^uint64(%s)", value[1:])
+					} else if ulong {
+						value = fmt.Sprintf("^uint32(%s)", value[1:])
+					} else {
+						value = fmt.Sprintf("^%s", value[1:])
+					}
+				}
+				data = &ConstData{
+					Name:  &ExportTranslator{&IdentifierTranslator{v.E.Name}},
+					Value: &ValueTranslator{value},
+				}
+			} else if len(v.E.Name) > 0 && len(v.E.Alias) > 0 {
+				data = &ConstData{
+					Name:  &ExportTranslator{&IdentifierTranslator{v.E.Name}},
+					Value: &ExportTranslator{&IdentifierTranslator{v.E.Alias}},
+				}
+			}
+			if data != nil {
+				list = append(list, *data)
+			}
+		}
+	}
+	return &struct {
+		Template string
+		Data     []ConstData
+	}{"const", list}
 }
